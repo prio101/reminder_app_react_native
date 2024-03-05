d83762c94b054be61dbd73ea63a3d12c268b5050
Mahabub Islam <miprio101@gmail.com>
2024-03-05 19:01:02 +0600

    Feat: Show last third time for the Tahazzud

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [dayOfMonth, setDayOfMonth] = useState(null);
+  const [currentMonth, setCurrentMonth] = useState(null);
+  const [currentYear, setCurrentYear] = useState(null);
+  useEffect(() => {
+    const dayOfMonth = new Date().getDate();
+    dayOfMonth && setDayOfMonth(dayOfMonth);
+    const currentMonth = new Date().getMonth() + 1;
+    currentMonth && setCurrentMonth(currentMonth);
+    const currentYear = new Date().getFullYear();
+    currentYear && setCurrentYear(currentYear);
+    const city = 'Dhaka';
+    const country = 'Bangladesh';
+    const method = 3; // World Organization of Muslim League
+
+    const fetchData = async () => {
+        try {
+          let url = `http://api.aladhan.com/v1/calendarByCity/${currentYear}/${currentMonth}?city=${city}&country=${country}&method=${method}`;
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json'
+            }
+          });
+          const data = await response.json();
+          setApiData(data);
+        } catch (error) {
+          console.error(error);
+        }
+    }
+    fetchData();
+  }, []);
+  console.log(apiData)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>For Tahazzud Salat:</Text>
+      <Text style={styles.font}> Today the 1/3 (Lastthird) part of night will be at: </Text>
+        {apiData && <Text style={styles.font}>{apiData.data[dayOfMonth-1].timings.Lastthird}</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +52,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  font: {
+    fontSize: 15,
+    color: '#777'
+  }
 });
